c17d32f6feaa73662f393a28aa64325c5d750fb4
Rafał Szczepanik <rafal.sz@tutanota.com>
2024-10-09 12:25:35 +0200

    fix: add git plugin

--- release.config.cjs
+++ release.config.cjs
@@ -6,12 +6,8 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: false,
-      },
-    ],
+    "@semantic-release/npm",
     "@semantic-release/github",
+    "@semantic-release/git",
   ],
 };
